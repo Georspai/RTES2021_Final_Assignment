d216271a349b22f38eb91bd11dffd4af3b8f88a2
Georspai <81882727+Georspai@users.noreply.github.com>
2021-10-10 22:08:05 +0300

    Add files via upload

---
+++ covidTrace.c
@@ -0,0 +1,359 @@
+//RTES 2021
+//SPAIAS GEORGIOS
+//AEM: 8910
+
+#include "CovidTrace.h"
+
+#define FIN_TIME 25920 // in seconds
+#define TEST_TIME 144 // in seconds
+#define BT_EXPIRE_TIME 12
+#define CLC_EXPIRE_TIME 12096
+#define MIN_CLC_TIME 2.4
+
+
+
+pthread_mutex_t  *timer_lock, *timer_lock_2 , *q_lock;
+pthread_cond_t   *timer_cond_bt ,*timer_cond_covT ;
+
+bool isTimerFinished;
+
+FILE *f1;
+FILE *f2;
+
+int main(){
+
+    queue * BT_queue=queueInit();
+    if (BT_queue==NULL)
+    {   fprintf(stderr, "main: BT_Queue Init failed.\n");
+        exit(1);
+    }
+
+    queue * CLC_queue=queueInit();
+    if (CLC_queue==NULL)
+    {   fprintf(stderr, "main: CLC_Queue Init failed.\n");
+        exit(1);
+    }
+
+    f1 = fopen("BTnearMe.bin", "wb");
+    if (f1 == NULL) {
+    printf("Error opening file1!\n");
+    exit(1);
+    }
+    f2 = fopen("CLC_upload.txt", "w");
+    if (f2 == NULL) {
+    printf("Error opening file2!\n");
+    exit(1);
+    }
+    fprintf(f2,"Close Contact Upload file \n");
+
+    struct pthread_data pth_data;
+
+    pth_data.BT_queue= BT_queue;
+    pth_data.CLC_queue= CLC_queue;
+
+    pth_data.BT_exp_time= BT_EXPIRE_TIME;
+    pth_data.CLC_exp_time=CLC_EXPIRE_TIME;
+
+    pthread_t tid_1, tid_2;
+
+    timer_lock=(pthread_mutex_t*)malloc(sizeof(pthread_mutex_t));
+    pthread_mutex_init(timer_lock, NULL);
+
+    timer_lock_2=(pthread_mutex_t*)malloc(sizeof(pthread_mutex_t));
+    pthread_mutex_init(timer_lock_2, NULL);
+
+    q_lock=(pthread_mutex_t*)malloc(sizeof(pthread_mutex_t));
+    pthread_mutex_init(q_lock, NULL);
+
+    timer_cond_bt=(pthread_cond_t*)malloc(sizeof(pthread_cond_t));
+    pthread_cond_init(timer_cond_bt, NULL );
+
+    timer_cond_covT=(pthread_cond_t*)malloc(sizeof(pthread_cond_t));
+    pthread_cond_init(timer_cond_covT, NULL );
+
+    if(pthread_create(&tid_1,NULL,bt_thr_routine, &pth_data)!=0){
+        perror("Error: could not create thread 1");
+    }
+    if(pthread_create(&tid_2,NULL,covT_thr_routine, &pth_data)!=0){
+        perror("Error: could not create thread 2");
+    }
+
+    u_int count=0;
+    isTimerFinished=false;
+    Start=tic();
+    while (count<(FIN_TIME*10))
+    {   pthread_cond_signal(timer_cond_bt);
+        if (count%(TEST_TIME*10)==0)
+        {
+           pthread_cond_signal(timer_cond_covT);
+        }
+        count++ ;
+        usleep(100000);
+    }
+    isTimerFinished=true;
+    sleep(2);
+    pthread_cond_signal(timer_cond_covT);
+    sleep(2);
+    pthread_cond_signal(timer_cond_bt);
+    //printf("\n Timer Finished\n");
+
+
+    if(pthread_join(tid_1,NULL)!=0)
+    {
+        perror("failed to join thread 1");
+    }
+     if(pthread_join(tid_2,NULL)!=0)
+    {
+        perror("failed to join thread 2");
+    }
+
+    pthread_mutex_destroy(timer_lock);
+    pthread_mutex_destroy(timer_lock_2);
+    pthread_mutex_destroy(q_lock);
+    pthread_cond_destroy(timer_cond_bt);
+    pthread_cond_destroy(timer_cond_covT);
+
+    fclose(f1);
+    fclose(f2);
+
+    free(BT_queue);
+    free(CLC_queue);
+    return 0;
+}
+
+struct timeval tic()
+{
+  struct timeval tv;
+  gettimeofday(&tv,NULL);
+  return tv;
+}
+
+double toc(struct timeval begin)
+{
+  struct timeval end;
+  gettimeofday(&end, NULL);
+  double stime=((double)end.tv_sec-(double)begin.tv_sec)*1000 +
+                ((double)end.tv_usec-(double)begin.tv_usec)/1000 ;
+  stime= stime/1000;
+  size_t fw=fwrite(&stime,sizeof(stime),1,f1);
+  return stime;
+}
+
+void * bt_thr_routine(void * args)
+{   struct pthread_data *pth=(struct pthread_data *)args;
+    queue * BT_queue=pth->BT_queue;
+    queue * CLC_queue=pth->CLC_queue;
+    int bt_exp_t=pth->BT_exp_time;
+    int clc_exp_t=pth->CLC_exp_time;
+    u_int16_t *freq_t=pth->freq_mat;
+
+    int count=0;
+    macaddress mac_in;
+    double time_in=0;
+
+    while(1){
+        pthread_cond_wait(timer_cond_bt,timer_lock);
+        if (isTimerFinished)
+        {
+            break;
+        }
+        macaddress mac_in=BTnearMe();
+        time_in=toc(Start);
+        queueAdd(BT_queue,mac_in,time_in);
+        queueUpdate(BT_queue,bt_exp_t);
+
+        pthread_mutex_lock(q_lock);
+        closeContactsAdd(BT_queue,CLC_queue);
+        clc_freq(CLC_queue,freq_t);
+        removeDuplicates(CLC_queue,freq_t);
+        queueUpdate(CLC_queue,clc_exp_t);
+        pthread_mutex_unlock(q_lock);
+
+        count++;
+       // printf("%d mac:0:0:0:0:%X:%X time: %f \n", count,BT_queue->mac_buffer[BT_queue->tail-1].bytes[1],BT_queue->mac_buffer[BT_queue->tail-1].bytes[0], time_in);
+    }
+}
+
+void * covT_thr_routine(void * args)
+{   struct pthread_data *pth=(struct pthread_data *)args;
+    queue * clc_queue= pth->CLC_queue ;
+    u_int16_t *freq_t=pth->freq_mat;
+    while (1)
+    {
+        pthread_cond_wait(timer_cond_covT,timer_lock_2);
+        if (isTimerFinished)
+        {
+            break;
+        }
+        if (testCOVID()==1)
+        { pthread_mutex_lock(q_lock);
+          uploadContacts(clc_queue->mac_buffer , clc_queue->entries);
+	  pthread_mutex_unlock(q_lock);
+        }
+    }
+    return (NULL);
+}
+
+
+macaddress BTnearMe()
+{
+    macaddress mac ;
+    mac.bytes[0]=(unsigned char) (rand()%10);
+    mac.bytes[1]=(unsigned char) (rand()%10);
+    for (size_t i = 2; i < 6; i++)
+    {
+      mac.bytes[i]=0;
+    }
+    return mac;
+}
+
+bool testCOVID()
+{
+    u_int8_t neg_prob=rand()%5; // probability for positive test 1/5
+    if (neg_prob>0)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+
+void uploadContacts(macaddress* mac_in ,int num)
+{
+    macaddress * mac;
+    //mac=(macaddress *)malloc(num*sizeof(macaddress));
+    mac=mac_in;
+
+    fprintf(f2,"\n%d :",num);
+    for (int i = 0; i < num; i++)
+    {
+    fprintf(f2," ");
+    for (size_t k = 0; k <6; k++)
+    {
+       fprintf(f2,"%X",mac[i].bytes[5-k]);
+    }
+  }
+}
+
+
+queue * queueInit()
+{
+    queue *(q);
+    q=(queue *)malloc(sizeof(queue));
+    if (q==NULL)
+    {
+        return NULL ;
+    }
+    q->head=0;
+    q->tail=0;
+    q->entries=0;
+    q->added=0;
+
+    return q;
+}
+
+void queueAdd(queue * q, macaddress mac_in, double time_in)
+{
+    for (size_t i = 0; i < 6; i++)
+    {
+        *(q->mac_buffer[q->tail].bytes+i)=mac_in.bytes[i];
+    }
+    q->time_buffer[q->tail]=time_in;
+    q->tail++;
+    q->entries++;
+    q->added++;
+    if (q->tail==QUEUESIZE)
+    {
+        q->tail=0;
+    }
+    if (q->entries==QUEUESIZE)
+    {
+        printf("Warning: Queue is full!\n");
+    }
+}
+
+void queueUpdate(queue *q,int exp_time)
+{   int16_t last;
+    if (q->tail==0){ last=QUEUESIZE-1; }
+    else{ last=q->tail-1; }
+
+    if (q->time_buffer[last]-q->time_buffer[q->head]>=exp_time)
+    {   //printf("REMOVING: %d \n",q->head);
+        q->head++ ;
+        q->entries-- ;
+        if(q->head==QUEUESIZE)
+        {
+            q->head=0;
+        }
+        if (q->entries==0)
+        {
+            printf("Warning: Queue is empty!\n");
+        }
+    }
+}
+
+void  closeContactsAdd(queue * q, queue * clc_q)
+{
+     while (q->added>0)
+    {
+      int i=q->head;
+      while (i!=q->tail)
+      {
+        if (memcmp(q->mac_buffer[q->tail-1].bytes,q->mac_buffer[i].bytes,6)==0)
+        {
+          if(q->time_buffer[q->tail-1]- q->time_buffer[i]>MIN_CLC_TIME){
+            queueAdd(clc_q,q->mac_buffer[q->tail-1],q->time_buffer[q->tail-1]);
+            // printf("NEW CLOSE CONTACT %d:\n",clc_q->entries);
+            // printf("mac:0:0:0:0:0:%X time: %f   %d\n",q->mac_buffer[q->tail-1].bytes[0],q->time_buffer[q->tail-1],(q->tail-1));
+            // printf("mac:0:0:0:0:0:%X time: %f   %d\n",q->mac_buffer[i].bytes[0],q->time_buffer[i],i);
+            break;
+          }
+        }
+        i++;
+        if (i==QUEUESIZE) i=0;
+      }
+      q->added--;
+    }
+}
+
+void clc_freq(queue * q , u_int16_t *freq )
+{
+    int last;
+    if (q->tail!=0) { last=q->tail-1;}
+    else{ last=QUEUESIZE -1; }
+    freq[last]=0;
+    int i=q->head;
+    while (i!=q->tail)
+    {
+        if (memcmp(q->mac_buffer[last].bytes,q->mac_buffer[i].bytes,6)==0) { freq[i]++; }
+        i++;
+        if (i==QUEUESIZE) {  i=0;}
+    }
+}
+
+void removeDuplicates(queue * q,u_int16_t *freq)
+{
+    int i=q->head;
+    int j=i;
+    while(i!=q->tail)
+    {
+        if (freq[i]==1)
+        {
+            memcpy(q->mac_buffer[j].bytes , q->mac_buffer[i].bytes,6);
+            q->time_buffer[j]=q->time_buffer[i];
+            freq[j]=freq[i] ;
+            j++;
+            if (j==QUEUESIZE){ j=0 ;}
+        }else
+        {
+            freq[i]=0;
+            q->entries--;
+        }
+        i++;
+        if (i==QUEUESIZE){ i=0 ;}
+    }
+    q->tail=j;
+    //printf("ENTRIES LEFT %d \n",j);
+}
---
+++ covidTrace.h
@@ -0,0 +1,125 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <stdbool.h>
+#include <sys/time.h>
+#include <string.h>
+
+#define QUEUESIZE 1000
+
+/*Struct: macaddress
+* Struct to store the 6 bytes (48bits) of  a macaddresses
+*/
+typedef struct macaddress {
+    u_char bytes[6];
+} macaddress;
+
+/*Struct: queue
+* Struct to implement queues
+*/
+typedef struct queue{
+    macaddress mac_buffer[QUEUESIZE];
+    double     time_buffer[QUEUESIZE];
+    u_int16_t head,tail,entries,added;
+} queue;
+
+
+
+/*Struct: pthread_data
+* Struct to store the data that are passed into the thread routines
+*/
+struct pthread_data
+{
+    queue * BT_queue;
+    queue * CLC_queue;
+    u_int16_t freq_mat[QUEUESIZE];
+    int BT_exp_time;
+    int CLC_exp_time;
+} pthread_data;
+
+struct timeval Start;
+
+/*Function: BTnearMe
+*   Returns a  macaddress 0:0:0:0:X:Y where X,Y are random ints between 0 and 9.
+*   (Substitute for function that returns the macadress of a bluetooth device in close proximity)
+*/
+macaddress BTnearMe();
+
+/*Function: testCovid
+*   Returns TRUE when rand%X==0 and FALSE when rand%X>0 where X is a number
+*   we chose to give certain probability to simulate the results of a covid test.
+*   (Substitute for function that returns the TRUE if the user's covid Test is positive)
+*/
+bool testCOVID();
+
+/*Function: uploadContacts
+*   Writes the mac adresses of the user's close contactts and the number of close contacts in
+*   a .txt file (CLC_upload.txt)
+*   (Substitute for function that uploads the close contacts' mac addresses to some corresponding service)
+*
+*   Inputs: mac address array of close contacts ( macaddress* mac)
+*           the number of the user's close contacts
+*/
+void uploadContacts(macaddress* mac ,int num);
+
+/*Function: clc_freq
+*   Computes and updates the frequency that its value exists in the clc_queue
+*
+*   Inputs: queue which we wanna compute the frequency ( queue * q)
+*           Table of the frequencies corresponding to the input queue (uint_16t *freq)
+*/
+void clc_freq(queue * q , u_int16_t *freq );
+
+/*Function: removeDuplicates
+*   Deletes duplicate eentries of the input queue based on their frequency
+*
+*   Inputs: queue which we examine ( queue * q)
+*           Table of the frequencies corresponding to the input queue (uint_16t *freq)
+*/
+void removeDuplicates(queue * q,u_int16_t *freq);
+
+struct timeval tic();
+
+double toc(struct timeval begin);
+
+/*Function: bt_thr_routine
+*   Routine of the first thread (tid_1)
+*/
+void* bt_thr_routine(void* args);
+
+/*Function: covT_thr_routine
+*   Routine of the first thread (tid_2)
+*/
+void* covT_thr_routine(void* args);
+
+
+/*Function: queueInit
+*  Initializes the queue
+*/
+queue * queueInit();
+
+/*Function: queueAdd
+*   Adds a new entry in the tail of the queue
+*
+*   Inputs: the queue in which we add the new entry (queue * q)
+*           macaddress data of new entry            (macaddress * mac_in)
+*           time data of new entry                  ( double time_in)
+*/
+void queueAdd(queue * q,macaddress mac_in, double time_in);
+
+/*Function: queueUpdate
+*   removes the head entry of the queue if it has expired
+*
+*   Inputs: the queue we examine                     (queue * q)
+*           expiration time specific to the queue    (int exp_time)
+*/
+void queueUpdate(queue *q,int exp_time);
+
+/*Function: closeContactsAdd
+*   checks if the tail entry of q qualifies as a cloes contact and if so
+*   adds it to the clc_queue
+*   Inputs: the queue we examine                     (queue * q)
+*           the queue we add to                      (queue * clc_q)
+*/
+void closeContactsAdd(queue * q,queue *clc_q);
